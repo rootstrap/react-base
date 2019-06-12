24c80b64c5d7e4206a3e85578d8c2119b0e649b0
felipegarcia92 <felipe@rootstrap.com>
2019-06-12 17:28:36 -0300

    Removed reselect usage on useSession selector

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -1,15 +1,9 @@
 import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 
-const selectSession = createSelector(
-  state => state.session,
-  session => ({
-    authenticated: session.authenticated,
-    checked: session.checked,
-    user: session.user,
-  })
-);
-
-const useSession = () => useSelector(selectSession);
+const useSession = () => useSelector(({ session }) => ({
+  authenticated: session.authenticated,
+  checked: session.checked,
+  user: session.user,
+}));
 
 export default useSession;
