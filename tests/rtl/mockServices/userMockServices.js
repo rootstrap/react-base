2cf9b83e5038d0ecc7143d07ff78246c646397f4
Nicolas Santos Reyes <nicolassantosreyes@Nicolass-MacBook-Pro.local>
2021-03-19 00:59:21 -0300

    Add msw warnings for unhandling requests

--- tests/rtl/mockServices/userMockServices.js
+++ tests/rtl/mockServices/userMockServices.js
@@ -4,7 +4,6 @@ import faker from 'faker';
 import userFactory from 'fixtures/userFactory';
 
 const mockLoginRequest = rest.post(/\/users\/sign_in/i, async (req, res, ctx) => {
-  console.info('Pass API');
   const user = userFactory({ overrides: { ...req.body.user } });
   return res(
     ctx.set({
--- tests/rtl/setup/setUpTests.js
+++ tests/rtl/setup/setUpTests.js
@@ -3,7 +3,6 @@ import 'jest-axe/extend-expect';
 
 import { server } from 'rtl/mockServices/mockServer';
 
-console.info('Setup tests!');
 // MSW Server config
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'warn' });
